5da88c317c2820cae730af6a80deccaccb069d1d
MelvinAnthony <23cp0310003@student.hindustanuniv.ac.in>
2024-12-02 21:51:40 +0530

    array

---
+++ src/array.ts
@@ -0,0 +1,15 @@
+/* 
+// Array
+
+const store: number[] = [1,2,3,4,5,6,7,8] 
+
+store.push(9)
+
+console.log(store)
+
+// access the different data types.
+
+store.push("8")
+store.push("me")
+
+console.log(store) */
