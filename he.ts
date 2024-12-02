7bb15337b2fc9dd838f97b4813ecafc92786551b
MelvinAnthony <23cp0310003@student.hindustanuniv.ac.in>
2024-12-02 21:11:44 +0530

    implicit and Explicit

---
+++ he.ts
@@ -0,0 +1,16 @@
+// Explicit declaration
+let firstName1: String = "melvin"
+
+// implicit declaration
+let lastName = "Anthony"
+
+
+firstName1 = "Arul"
+
+console.log(firstName1)
+
+console.log(lastName)
+
+
+
+
---
+++ src/helloworld.ts
@@ -0,0 +1,17 @@
+/* // Explicit declaration
+let firstName: String = "melvin"
+
+// implicit declaration
+let lastName = "Anthony"
+
+
+firstName = "Arul"
+
+console.log(firstName) */
+
+
+let firstName: string = "Dylan"; // type string
+
+firstName = "melvin"; // attempts to re-assign the value to a different type
+
+console.log(firstName);
