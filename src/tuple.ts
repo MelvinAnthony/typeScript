0793581a1910dd869febc89a80ba9357d8fdc6a6
MelvinAnthony <23cp0310003@student.hindustanuniv.ac.in>
2024-12-02 21:59:56 +0530

    tuples

---
+++ src/tuple.ts
@@ -0,0 +1,25 @@
+/* 
+// define tuple
+
+let tryTuple: [boolean, number, string];
+
+
+// instialize the tuple
+
+tryTuple = [true, 5, "melvin"]
+
+console.log("Tuple Values are: "+tryTuple) */
+
+/* 
+// read only tuple
+
+let tryTuple: readonly [number, boolean, string]
+
+console.log(tryTuple) */
+
+
+// Named Tuple
+
+const nameTuple: [x: number, y: boolean, z: string] = [24, true, "melvin"]
+
+
