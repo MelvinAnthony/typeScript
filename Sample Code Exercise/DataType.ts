906c80226b037176f6044b07256fb65daa5cb7e5
MelvinAnthony <23cp0310003@student.hindustanuniv.ac.in>
2024-12-03 19:40:15 +0530

    DataTypes

---
+++ Sample Code Exercise/DataType.ts
@@ -0,0 +1,37 @@
+/* let id: number;
+
+let fb_id: any; // let fb_id;
+
+id = 1234
+
+fb_id = "melvin"
+
+console.log(id, fb_id) */
+
+
+/* const firstName_me : String = "melvin"
+
+const lastName_me = "A"
+
+let isLoading: boolean;
+
+isLoading = true */
+
+
+/* let rool: string | number;  // or operation
+
+rool = "Melvin"
+
+console.log(rool)
+ */
+
+
+/* let device: String | number | boolean;
+
+device = "melvin"
+console.log(device)
+device = 1234
+console.log(device)
+device = true
+console.log(device) */
+
